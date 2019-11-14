cc663d1c8d6a1c132bf35bd8fd7730ade60e9575
cegarciav <cegarcia2@uc.cl>
2019-11-14 22:15:23 +0100

    cambios show_trajectories.php

--- app/show_trajectories.php
+++ app/show_trajectories.php
@@ -1,21 +1,41 @@
 <?php
 
-  if (isset($_POST["nom_dep"]) &&
-    isset($_POST["nom_arr"]) &&
-    isset($_POST["date_dep"]) &&
-    isset($_POST["date_arr"])
+  if (
+      isset($_GET["nom_dep"]) &&
+      isset($_GET["nom_arr"]) &&
+      isset($_GET["date_dep"]) &&
+      isset($_GET["date_arr"]) &&
+      isset($_GET["heure"])
     )
   {
-    $start_name = $_POST["nom_dep"];
-    $stop_name = $_POST["nom_arr"];
-    $start_date = $_POST["date_dep"];
-    $stop_date = $_POST["date_arr"];
+    $start_name = $_GET["nom_dep"];
+    $stop_name = $_GET["nom_arr"];
+    $start_date = $_GET["date_dep"];
+    $stop_date = $_GET["date_arr"];
+    $minimum_time = $_GET["heure"];
+
+    require_once "../conexion.php";
+
+    if (mysqli_connect_error())
+    {
+      die("Erreur de connexion interne");
+    }
+
+    $query = "SELECT A.nom, G.ville, A.date "
+              ."FROM arret as A, gare as G "
+              ."WHERE G.nom = A.nom;";
+
+    $request = mysqli_query($conexion, $query);
+
+    while ($result = mysqli_fetch_assoc($request))
+    {
+      print_r($result);
+    }
+
   }
   else
   {
-    echo "How dare you modify my website?";
-
+    die("How dare you modify my website?");
   }
-
 
 ?>
