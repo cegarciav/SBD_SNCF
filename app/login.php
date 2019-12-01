bec1bd56fa363c99aa7bf924e9f470447c015f9c
= <=>
2019-12-01 18:32:05 +0100

    login para probar

---
+++ app/login.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title></title>
+</head>
+<body>
+
+	<div>
+		<form action="checklogin.php" method="POST">
+			<div>
+				<label for="uname"><b>User : </b></label>
+				<input type="text" placeholder="User" id="user" name="user" required></br>
+				<label for="uname"><b>Password : </b></label>
+				<input type="text" placeholder="Password" id="password" name="password" required></br>
+
+				<button type="submit">Login</button>
+			</div>
+		</form>
+	</div>
+
+</body>
+</html>
